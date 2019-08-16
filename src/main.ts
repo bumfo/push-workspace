a9a3d21cb3ad01538b39fc87dcd599f684e4c74d
bumfo <bumfo@users.noreply.github.com>
2019-08-16 23:22:10 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    core.warning(`Hello ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
   }
