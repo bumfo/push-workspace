d9177437d35290a6e6b06e44b4ef68407a2f6357
bumfo <bumfo@users.noreply.github.com>
2019-08-16 23:35:11 +0800

    Create push.sh

---
+++ src/push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+REF=$1
+if [ -z $1 ]; then
+    REF='dist'
+fi
+
+REMOTE_REPO="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -am "Pushed From Action ${GITHUB_ACTION}"
+git push ${REMOTE_REPO} :$REF
