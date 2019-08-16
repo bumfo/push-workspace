05f003ed5617ac3eb0a8819cbcd11810c14cd5b1
bumfo <bumfo@users.noreply.github.com>
2019-08-16 23:38:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
     core.warning(`Hello ${myInput}`);
+    
+    var myOutput = '';
+    var myError = '';
+
+    const options = {};
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        myError += data.toString();
+      }
+    };
+    options.cwd = './src';
+
+    await exec.exec('./push.sh', ['dist'], options);
+    
+    core.warning(`${myOutput}`);
+    core.error(`${myError}`);
   } catch (error) {
     core.setFailed(error.message);
   }
