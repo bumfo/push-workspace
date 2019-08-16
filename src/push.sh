8f0bfad187b22e6a95dadf4cb553a4653a6af877
bumfo <bumfo@users.noreply.github.com>
2019-08-17 00:53:53 +0800

    test

--- src/push.sh
+++ src/push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Hello World
-exit 1
+exit 0
 
 REF=$1
 if [ -z $1 ]; then
@@ -14,5 +14,5 @@ cd "${GITHUB_WORKSPACE}" || exit 1
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
-git commit -am "Pushed From Action ${GITHUB_ACTION}"
-git push ${REMOTE_REPO} :$REF
+git commit -am "Pushed From Action ${GITHUB_ACTION}" && git push ${REMOTE_REPO} :$REF
+exit $?
