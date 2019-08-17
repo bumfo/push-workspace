ea3b1de16abc938d4a1f67858a12b78a00d8013f
bumfo <bumfo@users.noreply.github.com>
2019-08-17 10:06:31 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,18 @@ import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 import path from 'path';
 
-import fs from 'fs';
-
-
-
 async function run() {
   try {
     var cwd = path.resolve(__dirname, '..')
 
-    fs.readdir(cwd, (err, files) => {
-      core.warning('' + err);
-      files.forEach(file => {
-        core.warning(file);
-      });
-    });
-
-    core.warning(`${__dirname}`);
-
-    const myInput = core.getInput('myInput');
-    core.warning(`Hello ${myInput}`);
+    const ref = core.getInput('ref');
+    core.warning(`Hello ${ref}`);
 
     const options = {
       cwd: cwd,
     };
 
-    await exec.exec(`${await io.which('bash', true)}`, ['src/push.sh', 'dist'], options);
+    await exec.exec(`${await io.which('bash', true)}`, ['src/push.sh', ref], options);
   } catch (error) {
     core.setFailed(error.message);
   }
