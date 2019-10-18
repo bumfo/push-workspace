84d01d2eb465afd4782226af0d5fbf779173d82e
bumfo <bumfo@users.noreply.github.com>
2019-10-18 09:36:40 +0800

    clean

--- src/push.sh
+++ src/push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-REF=$1
-if [ -z $1 ]; then
+REF="$1"
+if [ -z "${REF}" ]; then
     echo Empty ref specified
     exit 1
 fi
 
-if [ -z $GITHUB_TOKEN ]; then
+if [ -z "$GITHUB_TOKEN" ]; then
     echo Token invalid
     exit 1
 fi
@@ -19,8 +19,8 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 
 if git commit -am "Pushed From Action ${GITHUB_ACTION}"; then
-    git push ${REMOTE_REPO} HEAD:$REF
-    exit $?
+    git push "${REMOTE_REPO}" HEAD:"${REF}"
+    exit "$?"
 else
     echo Nothing to push
     exit 0
